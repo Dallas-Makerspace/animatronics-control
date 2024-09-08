abc77f51721416237e62dcaad35b309dc429f557
Adam Barrow <242196+excalbian@users.noreply.github.com>
2024-09-08 17:05:55 -0500

    feat: add stubbed settings page

---
+++ app/settings/page.tsx
@@ -0,0 +1,13 @@
+export default function SettingsPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <p>
+        Someday, this will be a settings page.
+      </p>
+      <div className="flex gap-4">
+        <button className="btn">Save</button>
+        <button className="btn">Cancel</button>
+      </div>
+    </main>
+  );
+}
